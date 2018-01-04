65d9358dc9ada6ca55e78f9824941b7ee07d1560
Pawel Mac <Pawel.Mac@gmail.com>
2018-01-04 18:03:38 +0100

    Create class RotateTiltLeftAction

---
+++ src/main/java/com/mycompany/virtual_camera/controller/rotation/RotateTiltLeftAction.java
@@ -0,0 +1,23 @@
+package com.mycompany.virtual_camera.controller.rotation;
+
+import com.mycompany.virtual_camera.model.ViewportModel;
+import java.awt.event.ActionEvent;
+import javax.swing.AbstractAction;
+
+/**
+ *
+ * @author Paweł Mac
+ */
+public class RotateTiltLeftAction extends AbstractAction {
+    
+    private final ViewportModel viewportModel;
+    
+    public RotateTiltLeftAction(ViewportModel viewportModel) {
+        this.viewportModel = viewportModel;
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        this.viewportModel.rotateTiltLeft();
+    }
+}
