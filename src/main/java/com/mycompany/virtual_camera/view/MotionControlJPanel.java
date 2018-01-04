9e4fe258a28416740db8e94bbc1c2ddedfc5c9c5
Pawel Mac <Pawel.Mac@gmail.com>
2018-01-04 14:17:04 +0100

    Create class MotionControlJPanel

---
+++ src/main/java/com/mycompany/virtual_camera/view/MotionControlJPanel.java
@@ -0,0 +1,99 @@
+package com.mycompany.virtual_camera.view;
+
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.TitledBorder;
+
+/**
+ *
+ * @author Paweł Mac
+ */
+public final class MotionControlJPanel extends JPanel {
+    
+    private final JLabel stepJLabel = new JLabel("step:");
+    private final JTextField stepJTextField = new JTextField(1);
+    
+    private final JButton moveForwardJButton  = new JButton();
+    private final JButton moveBackwardJButton = new JButton();
+    private final JButton moveLeftJButton     = new JButton();
+    private final JButton moveRightJButton    = new JButton();
+    private final JButton moveUpwardJButton   = new JButton();
+    private final JButton moveDownwardJButton = new JButton();
+    
+    public MotionControlJPanel() {
+        this.setBorder(new TitledBorder("Motion Control"));
+        this.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        this.add(stepJLabel, gbc);
+        gbc.fill = GridBagConstraints.BOTH;
+        this.add(stepJTextField, gbc);
+        JPanel buttonsJPanel = new JPanel(new GridBagLayout());
+        gbc.gridy = 1;
+        gbc.gridwidth = GridBagConstraints.REMAINDER;
+        this.add(buttonsJPanel, gbc);
+        gbc.gridwidth = 1;// back to default
+        gbc.gridy = 0;
+        gbc.gridx = 1;
+        buttonsJPanel.add(moveForwardJButton, gbc);
+        gbc.gridx = 3;
+        buttonsJPanel.add(moveUpwardJButton, gbc);
+        gbc.gridy = 1;
+        gbc.gridx = GridBagConstraints.RELATIVE;
+        buttonsJPanel.add(moveLeftJButton, gbc);
+        buttonsJPanel.add(moveBackwardJButton, gbc);
+        buttonsJPanel.add(moveRightJButton, gbc);
+        buttonsJPanel.add(moveDownwardJButton, gbc);
+        
+    }
+    
+    // Getters
+    
+    public JTextField getStepJTextField() {
+        return stepJTextField;
+    }
+    
+    public JButton getMoveForwardJButton() {
+        return moveForwardJButton;
+    }
+    
+    public JButton getMoveBackwardJButton() {
+        return moveBackwardJButton;
+    }
+    
+    public JButton getMoveLeftJButton() {
+        return moveLeftJButton;
+    }
+    
+    public JButton getMoveRightJButton() {
+        return moveRightJButton;
+    }
+    
+    public JButton getMoveUpwardJButton() {
+        return moveUpwardJButton;
+    }
+    
+    public JButton getMoveDownwardJButton() {
+        return moveDownwardJButton;
+    }
+    
+    // Test MotionControlJPanel
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame();
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.setMinimumSize(new Dimension(120, 90));
+        
+        JPanel mainJPanel = new JPanel();
+        jFrame.add(mainJPanel);
+        mainJPanel.add(new MotionControlJPanel());
+        
+        jFrame.pack();
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+    }
+}
