9b438065401c5f174c007e9a375f2782103d8705
Pawel Mac <Pawel.Mac@gmail.com>
2018-01-04 17:38:47 +0100

    Create class MoveUpwardAction

---
+++ src/main/java/com/mycompany/virtual_camera/controller/motion/MoveUpwardAction.java
@@ -0,0 +1,23 @@
+package com.mycompany.virtual_camera.controller.motion;
+
+import com.mycompany.virtual_camera.model.ViewportModel;
+import java.awt.event.ActionEvent;
+import javax.swing.AbstractAction;
+
+/**
+ *
+ * @author Paweł Mac
+ */
+public class MoveUpwardAction extends AbstractAction {
+    
+    private final ViewportModel viewportModel;
+    
+    public MoveUpwardAction(ViewportModel viewportModel) {
+        this.viewportModel = viewportModel;
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        this.viewportModel.moveUpward();
+    }
+}
